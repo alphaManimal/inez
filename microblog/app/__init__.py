df3da0e0808db20e39814d155e4f93e93953cf43
Nate Housley <natehousley@zosoIV-2.local>
2015-10-22 15:00:04 -0600

    finished lesson 5

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,19 +1,17 @@
 import os
 from flask.ext.login import LoginManager
 from flask.ext.openid import OpenID
-oid = OpenID(app, '~/inez/microblog/flask/flask_openid.py', safe_roots=[])
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 from config import basedir
+
+app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
 
 lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
 oid = OpenID(app, os.path.join(basedir, 'tmp'))
-
-from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config.from_object('config')
-db = SQLAlchemy(app)
 
 from app import views, models
--- microblog/app/views.py
+++ microblog/app/views.py
@@ -1,10 +1,36 @@
-from flask import render_template, flash, redirect, session, url_for, request g
+from flask import render_template, flash, redirect, session, url_for, request, g
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from app import app, db, lm, oid
 from .forms import LoginForm
 from .models import User
 
-# index view function suppressed for brevity
+@lm.user_loader
+def load_user(id):
+	return User.query.get(int(id))
+
+@app.before_request
+def before_request():
+	g.user = current_user
+
+@app.route('/')
+@app.route('/index')
+@login_required
+def index():
+	user = g.user
+	posts = [
+		{
+			'author': {'nickname': 'John'},
+			'body': 'Beautiful day in Portland!'
+		},
+		{
+			'author': {'nickname': 'Susan'},
+			'body': 'The Avengers movie was so cool!'
+		}
+	]
+	return render_template('index.html',
+				title='Home',
+				user=user,
+				posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 @oid.loginhandler
@@ -19,17 +45,6 @@ def login():
 			title = 'Sign In',
 			form=form,
 			providers=app.config['OPENID_PROVIDERS'])
-		flash('Login requested for OpenID="%s", remember_me=%s' %
-			(form.openid.data, str(form.remember_me.data)))
-		return redirect('/index')
-	return render_template('login.html',
-				title='Sign In',
-				form=form,
-				providers=app.config['OPENID_PROVIDERS'])
-
-@app.before_request
-def before_request():
-	g.user = current_user
 
 @oid.after_login
 def after_login(resp):
@@ -49,30 +64,9 @@ def after_login(resp):
 		remember_me = session['remember_me']
 		session.pop('remember_me', None)
 	login_user(user, remember = remember_me)
-	return redirect(request.args.get('next') or url_for('index')
+	return redirect(request.args.get('next') or url_for('index'))
 
-@app.route('/')
-@app.route('/index')
-@login_required
-def index():
-	user = g.user
-	posts = [
-		{
-			'author': {'nickname': 'John'},
-			'body': 'Beautiful day in Portland, Maine!'
-		},
-		{
-			'author': {'nickname': 'Susan'},
-			'body': 'The Avengers movie was so shit!'
-		}
-	]
-	return render_template('index.html',
-				title='Home',
-				user=user,
-				posts=posts)
-
-@lm.user_loader
-def load_user(id):
-	return User.query.get(int(id))
-
-
+@app.route('/logout')
+def logout():
+	logout_user()
+	return redirect(url_for('index'))
