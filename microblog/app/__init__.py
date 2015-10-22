5aef97c92812412974cec612345a11cd935a71cb
Nate Housley <natehousley@zosoIV-2.local>
2015-10-21 20:03:51 -0600

    no openid module

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,3 +1,14 @@
+import os
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+oid = OpenID(app, '~/inez/microblog/flask/flask_openid.py', safe_roots=[])
+from config import basedir
+
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
+
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
