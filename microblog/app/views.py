d089f53788687c48f442fedbbe15327f4028d84f
Nate Housley <natehousley@zosoIV-2.local>
2015-10-27 20:18:35 -0600

    lession VII

--- microblog/app/views.py
+++ microblog/app/views.py
@@ -55,7 +55,8 @@ def after_login(resp):
 		nickname = resp.nickname
 		if nickname is None or nickname == "":
 			nickname = resp.email.split('@')[0]
-		user = User(nickname=nickname, email=resp.email)
+		nickname = User.make_unique_nickname(nickname)
+		user = User(nickname = nickname, email = resp.email)
 		db.session.add(user)
 		db.session.commit()
 	remember_me = False
@@ -68,7 +69,7 @@ def after_login(resp):
 @app.route('/edit', methods=['GET', 'POST'])
 @login_required
 def edit():
-	form = EditForm()
+	form = EditForm(g.user.nickname)
 	if form.validate_on_submit():
 		g.user.nickname = form.nickname.data
 		g.user.about_me = form.about_me.data
@@ -100,3 +101,12 @@ def user(nickname):
 	return render_template('user.html',
 				user=user,
 				posts=posts)
+
+@app.errorhandler(404)
+def not_found_error(error):
+	return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+	db.session.rollback()
+	return render_template('500.html'), 500
